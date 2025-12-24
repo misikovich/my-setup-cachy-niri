869e30b48769e515d8c8b133517041cfefa5377b
misikovich <misikovich@gmail.com>
2025-12-24 08:13:06 +0100

    fixed setwp script

--- collect.sh
+++ collect.sh
@@ -7,9 +7,9 @@ mkdir -p files/.config/niri/
 mkdir -p files/.config/fastfetch/
 mkdir -p files/Pictures/Wallpapers
 
-cp /home/misikovich/.config/alacritty/alacritty.toml files/.config/alacritty.toml
-cp /home/misikovich/.config/fastfetch/fastfetch.jsonc files/.config/fastfetch/fastfetch.jsonc
-cp /home/misikovich/.config/fish/config.fish files/.config/fish/config.fish
-cp /home/misikovich/.nanorc files/.nanorc
-cp /home/misikovich/.config/niri/config.kdl files/.config/niri/config.kdl
-cp /home/misikovich/Pictures/Wallpapers/* files/Pictures/Wallpapers/*
+cp -f /home/misikovich/.config/alacritty/alacritty.toml files/.config/alacritty.toml
+cp -f /home/misikovich/.config/fastfetch/fastfetch.jsonc files/.config/fastfetch/fastfetch.jsonc
+cp -f /home/misikovich/.config/fish/config.fish files/.config/fish/config.fish
+cp -f /home/misikovich/.nanorc files/.nanorc
+cp -f /home/misikovich/.config/niri/config.kdl files/.config/niri/config.kdl
+cp -f /home/misikovich/Pictures/Wallpapers/* files/Pictures/Wallpapers/*
